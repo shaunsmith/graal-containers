95a8ce70c5f6166d861874c7a3dc7e77b742424c
Sachin Pikle <sachin.pikle@oracle.com>
2022-04-15 10:54:22 +0530

    Updated Step1 to use multistage Native Image build

--- step1.sh
+++ step1.sh
@@ -2,19 +2,15 @@
 
 REPO=localhost
 APP=jibber
-TYPE=native
-VER=0.1
-CONTAINER=${REPO}/${APP}:${TYPE}.${VER}
+TYPE=gvmee-native
+VER=0.0.1
+TAG=${REPO}/${APP}:${TYPE}.${VER}
 APP_FILE=jibber
-echo "Container : ${CONTAINER}"
+echo "Tag : ${TAG}"
 
-echo "Building native executable..."
-mvn package -DskipTests -Pnative
-echo "DONE"
-
-echo "Dockerizing our app.."
+echo "Build the app JAR, generate the app native executable and dockerize our app native executable."
 docker login container-registry.oracle.com
-docker build -f ./Dockerfiles/Dockerfile.native \
+docker build -f ./Dockerfiles/multistage/Dockerfile.gvmee-ni \
              --build-arg APP_FILE=${APP_FILE} \
-             -t ${CONTAINER} .
+             -t ${TAG} .
 echo "DONE"
