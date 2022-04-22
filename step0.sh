bb3ab8614fd0bfe0c373d29b92e0f61f13892fd1
Sachin Pikle <sachin.pikle@oracle.com>
2022-04-22 20:36:30 +0530

    Updated JDK build relates files

--- step0.sh
+++ step0.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
-REPO=localhost
-APP=jibber
-TYPE=gvmee-jdk17
-VER=0.0.1
-TAG=${REPO}/${APP}:${TYPE}-${VER}
-JAR_FILE=${APP}-${VER}-SNAPSHOT-exec.jar
+TAG="jibber-jar-gvmee2130-jdk17:0.0.1"
+JAR_FILE="jibber-0.0.1-SNAPSHOT-exec.jar" # Must match the value in pom.xml
 
 echo "Tag : ${TAG}"
 echo "Running multistage build to build the JAR and dockerize our app JAR."
