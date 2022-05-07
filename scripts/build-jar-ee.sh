80c0621dc31646829964b553bd4278424b9218f0
Sachin Pikle <sachin.pikle@oracle.com>
2022-05-07 12:08:07 +0530

    Updated

--- scripts/build-jar-ee.sh
+++ scripts/build-jar-ee.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Run this script from the project's root directory containing src and pom.xml
 
-TAG="jibber-jar-gvmee2130-jdk17:0.0.1"
+TAG="jibber-jar-gvmee22-jdk17:0.0.1"
 JAR_FILE="jibber-0.0.1-SNAPSHOT-exec.jar" # Must match the value in pom.xml
 
 echo "Tag : ${TAG}"
